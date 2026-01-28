0c1c250043b573f7d7dfd0d033b3b22305b80d9e
= <muhammadriyask11@gmail.com>
2026-01-29 01:02:46 +0530

    build: add exact timers for production - fixed tank low issue

--- main.c
+++ main.c
@@ -132,9 +132,9 @@ unsigned char dry_run_check(void)
     
     if (!motor_on) { return 0; }
     
-    if (dry_run_latched && dry_run_timer >= 1000) { return 1;} // Lost to dry run after 
+    if (dry_run_latched && dry_run_timer >= 4000) { return 1;} // Lost to dry run after 
 
-    if (dry_run_timer >= 3000) { return 1; } // Dry run detected
+    if (dry_run_timer >= 30000) { return 1; } // Dry run detected
     
     if (DRY_RUN) { dry_run_timer++; }  // increase timer in DRY RUN not triggered
     else { dry_run_timer = 0; dry_run_latched = 1; }
@@ -185,7 +185,7 @@ void main(void)
     init_hw();
     
     // Delay before starting
-    __delay_ms(5000); alarm();
+    __delay_ms(20000); alarm();
     
     while(1)
     {
@@ -217,7 +217,7 @@ void main(void)
             LED_TANK_FULL = 1;
             alarm();
 
-            __delay_ms(10000); // no loop delay, 10s
+            __delay_ms(20000); // no loop delay, 30s
             LED_TANK_FULL = 0;
 
             reset_starter_relay();
