0f23170b7988d4178b85b4014c8d743b1e3893f0
= <muhammadriyask11@gmail.com>
2026-01-28 12:49:51 +0530

    fix: rename variable names

--- globals.h
+++ globals.h
@@ -4,8 +4,8 @@
 extern unsigned int blink_cnt;
 extern __bit blink_state;
 extern unsigned int motor_on;
-unsigned int r2_timer;
-unsigned char r2_state;
+unsigned int relay_timer;
+unsigned char relay_state;
 unsigned int dry_run_timer;
 unsigned int dry_run;
 
