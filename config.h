50d0f72ef21eeb4958c633f2cc2f3e91084f9bb4
= <muhammadriyask11@gmail.com>
2026-02-01 13:23:24 +0530

    chore: remove unnecessary includes and variables

--- config.h
+++ config.h
@@ -1,8 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-//#define PRODUCTION  // Comment this line to use TESTING LEVEL timer delays
-
 #define _XTAL_FREQ            8000000
 #define BLINK_TICKS           1
 #define TMR1_RELOAD           40536  // 100ms
--- delays.h
+++ delays.h
@@ -6,25 +6,21 @@
 
 #define DELAY_STARTER_RELAY     30     // 30 x 100ms = 3s
 #define DELAY_STARTED_RELAY_ON  30
-#define DELAY_LED_PUMP_ON       5
+#define DELAY_LED_PUMP_ON       5 
 
-
-#if defined(PRODUCTION)
 
 // PRODUCTION LEVEL TIMER DELAYS
 #define DELAY_START_UP          20000   // 20s
 #define DELAY_DRY_RUN           1500    // 2m 30s (150s x 100 tick)
 #define DELAY_DRY_RUN_AFTER     200     // 20s
 #define DELAY_DRY_RUN_RESET     36000   // 1h (600 (1m tick) x 60)
-#define DELAY_TANK_FULL         30000   // 30s
+#define DELAY_TANK_FULL         60000   // 1m
 
-#else   // TESTING
+// TESTING LEVEL TIMER DELAYS
+//#define DELAY_START_UP          5000   // 5s
+//#define DELAY_DRY_RUN           200    // 100 x 200ms = 20s
+//#define DELAY_DRY_RUN_AFTER     100    // 10s
+//#define DELAY_DRY_RUN_RESET     600    // 1m
+//#define DELAY_TANK_FULL         10000  // 10s
 
-#define DELAY_START_UP          5000   // 5s
-#define DELAY_DRY_RUN           200    // 100 x 200ms = 20s
-#define DELAY_DRY_RUN_AFTER     100    // 10s
-#define DELAY_DRY_RUN_RESET     600    // 1m
-#define DELAY_TANK_FULL         10000  // 10s
-
-#endif
 #endif
