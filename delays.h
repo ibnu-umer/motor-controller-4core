662d277fb56d3c461fe6aa3caa930bc91c1f94c2
= <muhammadriyask11@gmail.com>
2026-02-01 12:25:06 +0530

    refactor: modify delay timers

--- delays.h
+++ delays.h
@@ -4,29 +4,27 @@
 #ifndef DELAYS_H
 #define DELAYS_H
 
+#define DELAY_STARTER_RELAY     30     // 30 x 100ms = 3s
+#define DELAY_STARTED_RELAY_ON  30
+#define DELAY_LED_PUMP_ON       5
+
 
 #if defined(PRODUCTION)
 
 // PRODUCTION LEVEL TIMER DELAYS
-#define DELAY_START_UP          20000
-#define DELAY_STARTER_RELAY     600
-#define DELAY_STARTED_RELAY_ON  600
-#define DELAY_DRY_RUN           30000
-#define DELAY_DRY_RUN_AFTER     3000
-#define DELAY_DRY_RUN_RESET     30000
-#define DELAY_TANK_FULL         300000
+#define DELAY_START_UP          20000   // 20s
+#define DELAY_DRY_RUN           1500    // 2m 30s (150s x 100 tick)
+#define DELAY_DRY_RUN_AFTER     200     // 20s
+#define DELAY_DRY_RUN_RESET     36000   // 1h (600 (1m tick) x 60)
+#define DELAY_TANK_FULL         30000   // 30s
 
 #else   // TESTING
 
-#define DELAY_START_UP          5000
-#define DELAY_STARTER_RELAY     30     // 30 x 100ms = 3s
-#define DELAY_STARTED_RELAY_ON  30
+#define DELAY_START_UP          5000   // 5s
 #define DELAY_DRY_RUN           200    // 100 x 200ms = 20s
 #define DELAY_DRY_RUN_AFTER     100    // 10s
 #define DELAY_DRY_RUN_RESET     600    // 1m
-#define DELAY_TANK_FULL         15000
-#define DELAY_LED_PUMP_ON       5
+#define DELAY_TANK_FULL         10000  // 10s
 
 #endif
-
 #endif
