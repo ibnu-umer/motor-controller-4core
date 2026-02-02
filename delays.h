9cb2f0a42a7392c7979e0720ea8af9c4be10f892
= <muhammadriyask11@gmail.com>
2026-02-02 17:54:32 +0530

    build: change delays to testing level

--- delays.h
+++ delays.h
@@ -10,17 +10,17 @@
 
 
 // PRODUCTION LEVEL TIMER DELAYS
-#define DELAY_START_UP          20000   // 20s
-#define DELAY_DRY_RUN           1500    // 2m 30s (150s x 100 tick)
-#define DELAY_DRY_RUN_AFTER     200     // 20s
-#define DELAY_DRY_RUN_RESET     108000UL// 3h (600 (1m tick) x 180)
-#define DELAY_TANK_FULL         60000   // 1m
+//#define DELAY_START_UP          20000   // 20s
+//#define DELAY_DRY_RUN           1500    // 2m 30s (150s x 100 tick)
+//#define DELAY_DRY_RUN_AFTER     200     // 20s
+//#define DELAY_DRY_RUN_RESET     108000UL// 3h (600 (1m tick) x 180)
+//#define DELAY_TANK_FULL         60000   // 1m
 
 // TESTING LEVEL TIMER DELAYS
-//#define DELAY_START_UP          5000   // 5s
-//#define DELAY_DRY_RUN           200    // 100 x 200ms = 20s
-//#define DELAY_DRY_RUN_AFTER     100    // 10s
-//#define DELAY_DRY_RUN_RESET     600    // 1m
-//#define DELAY_TANK_FULL         10000  // 10s
+#define DELAY_START_UP          5000   // 5s
+#define DELAY_DRY_RUN           200    // 100 x 200ms = 20s
+#define DELAY_DRY_RUN_AFTER     100    // 10s
+#define DELAY_DRY_RUN_RESET     600    // 1m
+#define DELAY_TANK_FULL         10000  // 10s
 
 #endif
